52507324baee893c2b29e90c736c562a57e00d88
GtenIOS <jdevlani@yahoo.in>
2023-01-05 12:11:32 +0530

    Updated for latest version of Zig

--- fileio.zig
+++ fileio.zig
@@ -3,7 +3,7 @@ pub inline fn readFile(allocator: std.mem.Allocator, file_name: []const u8) ![]u
     return try std.fs.cwd().readFileAlloc(allocator, file_name, std.math.maxInt(usize));
 }
 
-pub inline fn readFileToLines(allocator: std.mem.Allocator, file_name: []const u8, array: *std.ArrayList([]const u8)) !void {
+pub inline fn readFileToLines(allocator: std.mem.Allocator, file_name: []const u8, array: *std.ArrayList([]const u8)) anyerror!void {
     const file = try std.fs.cwd().openFile(file_name, .{});
     while (file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize))) |line| {
         if (line) |value| {
@@ -21,6 +21,6 @@ pub inline fn readFileToLines(allocator: std.mem.Allocator, file_name: []const u
 pub inline fn writeBytesToFile(bytes: []const u8, file_name: []const u8) !void {
     const file = try std.fs.cwd().createFile(file_name, .{ .truncate = false });
     try file.writer().writeAll(bytes);
-    
+
     file.close();
 }
